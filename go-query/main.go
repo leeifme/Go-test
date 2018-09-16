e4d1ef0a04353b5215db375da2dd913a196d46de
李磊 <1369183590@qq.com>
2018-09-16 22:19:48 +0800

    project

--- go-query/main.go
+++ go-query/main.go
@@ -25,12 +25,13 @@ func ExampleScrape() {
 		log.Fatal(err)
 	}
 
-	// Find the review items
+	// #ms-album-reviews-id-2 > div > article:nth-child(2) > div.content-block > a
 	doc.Find(".sidebar-reviews article .content-block").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the band and title
 		band := s.Find("a").Text()
 		title := s.Find("i").Text()
-		fmt.Printf("Review %d: %s - %s\n", i, band, title)
+		helf, _ := s.Find("a").Attr("href")
+		fmt.Printf("Review %d: %s - %s - %s\n", i, band, title, helf)
 	})
 }
 
